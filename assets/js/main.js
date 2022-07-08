c9caea421c2c27049bc2e720ece365c3452b14d8
muhammetur <muhammetur@gmail.com>
2022-07-08 23:26:48 +0300

    Döviz Çevirme İşlemleri
    İnput alanına girilen değer anlık olarak diğer kurlara çeviriliyor ve selectbox'dan kur değişikliği yapılırsa seçilen kur değeri gösteriliyor. USD default seçili olarak getirildi ve değeri otomatik hesaplama yapılıp sonuç alanına yazdırıldı.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,33 @@
-var requestURL = 'https://api.exchangerate.host/latest?base=TRY&symbols=USD,EUR,JPY,GBP,DKK,NOK,TRY';
+var requestURL = 'https://api.exchangerate.host/latest?base=TRY&symbols=DKK,USD,EUR,JPY,GBP,NOK,TRY';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
+
+var currentCurreny = "TRY";
+var currentCurrenyPrice = 1;
+
 request.onload = function() {
   var response = request.response;
-  var currencyDate = request.response.date;
+  var currencyDate = request.response.date; // tarihi çekmek için tanımlanan değişken.
 
-  //console.log(Object.keys(response.rates)[0] );
+  $("#kur-select").empty(); // ilk açılışta kur seçimi yapılan seletbox içini boşaltıyoruz.
 
-  var keys_obje = Object.keys(response.rates)[0];
-
-  //console.log(keys_obje);
-  $("#kur-select").empty();
-  $.each(response.rates, function(key, val) {
-    $("#kur-select").append("<option value='" + val + "'>" + key + "</option>");
-    console.log(key, val);
+  $.each(response.rates, function(key, val) { //tüm kurları döndürüp selectin içine basıyoruz.
+    if(key != currentCurreny){
+      if(key == "USD"){ // kur USD yi default selected olarak getiriyoruz.
+      $("#kur-select").append("<option selected value='" + val + "'>" + key + "</option>");
+      }else{
+      $("#kur-select").append("<option  value='" + val + "'>" + key + "</option>");
+      }
+      console.log(key, val);
+    }
   });
 
 
-  var usdToTRY = 1 / response.rates.USD;
-  usdToTRY = usdToTRY.toFixed(3);
+  var usdToTRY = 1 / response.rates.USD; // bu işlemde kur değeri 1 e bölünerek gerçek kur bulunuyor. 
+  usdToTRY = usdToTRY.toFixed(3);// en fazla 3 rakamı alıyoruz
   var eurToTRY = 1 / response.rates.EUR;
   eurToTRY = eurToTRY.toFixed(3);
   var jpyToTRY = 1 / response.rates.JPY;
@@ -33,6 +39,7 @@ request.onload = function() {
   var nokToTRY = 1 / response.rates.NOK;
   nokToTRY = nokToTRY.toFixed(3);
 
+  $('#res').val(usdToTRY);//sonuç alanındaki değer default olarak usd to try gelmesi için bu değer atanıyor.
   $("#usd, #usd-2").html(usdToTRY);
   $("#eur, #eur-2").html(eurToTRY);
   $("#jpy, #jpy-2").html(jpyToTRY);
@@ -40,5 +47,16 @@ request.onload = function() {
   $("#dkk, #dkk-2").html(dkkToTRY);
   $("#nok, #nok-2").html(nokToTRY);
 
-  $("#date").html(response.date)
+  $("#date").html(currencyDate); //json dan gelen güncelleme tarihini date id li div e basıyoruz.
+
+  $('#kur-input').keyup(function(){ //input alanına girdikçe çalışıyor.
+    var value = $(this).val();
+    var select = $('#kur-select').val();
+    $('#res').val((value / select).toFixed(3));
+});
+$('#kur-select').change(function(){ // selectbox dan kur değiştirdikçe çalışıyor.
+  var value = $('#kur-input').val();
+  var select = $('#kur-select').val();
+  $('#res').val((value / select).toFixed(3));
+});
 }
